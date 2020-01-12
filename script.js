7b1ef379d65e036594efedad698ce62efe2cc59d
Jonathan Choi <Jonathan.Junghoon.Choi@gmail.Com>
2020-01-12 11:57:41 -0500

    scaffolding

---
+++ script.js
@@ -0,0 +1,7 @@
+var srch = document.getElementById("srch");
+var srchBtn = document.getElementById("srchBtn");
+var pstSrch = document.getElementById("pstSrch");
+var mainDisp = document.getElementById("mainDisp");
+var yest = document.getElementById("yest");
+var tod = document.getElementById("tod");
+var tom = document.getElementById("tom");
